65c688490798cfcbf2312976b799cc15933635bd
kamal <kamal.shadi85@gmail.com>
2013-11-14 21:51:28 -0500

    event detection added

--- eventDetection.py
+++ eventDetection.py
@@ -1,15 +1,78 @@
 import Queue as qx
-import pylab as pl
+#~ import pylab as pl
 import numpy as num
 from scipy.stats import t
 import math
+from numpy.random import randint,normal
 
+
+def eventVector(a,N):
+	# a is a tuple or list of int for shift position
+	if a==[]:
+		return [0]*N
+	if type(a[0]) == tuple :
+		x= [int(w[0]) for w in a]+[N]
+	else:
+		x=[int(w) for w in a]+[N]
+	e=0
+	ind=0
+	out=[0]*N
+	for k in x:
+		out[ind:k]=[e]*(k-ind)
+		if e==0:
+			e=1
+		else :
+			e=0
+		ind=k
+	return out
+	
+def invEvent(a):
+	# a is event vector
+	return([1-x for x in a])
+	
+def roc(a,b):
+	# a is real event vector
+	# b is algorithm detected
+	z=[a[i]*b[i] for i in range(len(a))]
+	tp=float(sum(z))/sum(a)
+	ap=invEvent(a)
+	z=[ap[i]*b[i] for i in range(len(b))]
+	fp=float(sum(z))/sum(ap)
+	return(fp,tp)
+	
+def genEvent(l,r,d,outlier=0):
+	# l is the length of the event
+	# r is the l/N where N is the length of the output ( r < 1/3 )
+	# d is the level of the event
+	if r > 0.3 :
+		print "please provide 0<r<0.3"
+		return None
+	sign=2*randint(2)-1
+	N=int(math.ceil(l/r))
+	a = normal(0,1,N)
+	i1=l+randint(N-2*l)
+	i2=i1+l
+	a[i1:i2]=[x+sign*d for x in a[i1:i2]]
+	# adding some outlier
+	m=int(math.ceil(outlier*N))
+	for j in range(m):
+		ind=randint(N)
+		sign=2*randint(2)-1
+		mag=sign*randint(1,5)
+		a[ind]=a[ind]+mag
+	ev=eventVector([i1,i2],N)
+	return [a,ev]
+	
+	
+	
+	
+		  
 def sigDiff(s,l,p=.01):
 	#calculate significant change for x with confidance p
 	tval=abs(t.ppf(p/2,l-1))
 	return tval*s*math.sqrt(2.0/l)
 	
-def eventDetection(a,sigma,l=30,p=0.01):
+def eventDetection(a,sigma=1,l=30,p=0.01):
 	N=len(a)
 	diff=sigDiff(sigma,l)
 	q=qx.Queue(N)
@@ -21,8 +84,8 @@ def eventDetection(a,sigma,l=30,p=0.01):
 		V[i]=q.get(False)
 	ev=qx.Queue(l)
 	i=l-1
-	pl.plot(range(N),a)
 	inp=qx.Queue(N)
+	out=[]
 	while ((not q.empty()) or (not inp.empty())) :
 		if V[-1] is not None:
 			if len(V)==l:
@@ -72,8 +135,9 @@ def eventDetection(a,sigma,l=30,p=0.01):
 						m=num.mean(V+temp)
 						for w in temp:
 							inp.put(w,False)
-						pl.plot(i,v,'r*')
+						#~ pl.plot(i,v,'r*')
 						ev=qx.Queue(l)
+						out.append((i,-RSI))
 
 				else:						 #upward shift
 					RSI=(v-m-diff)/(l*sigma)
@@ -105,8 +169,9 @@ def eventDetection(a,sigma,l=30,p=0.01):
 						m=num.mean(V+temp)
 						for w in temp:
 							inp.put(w,False)
-						pl.plot(i,v,'r*')
+						#~ pl.plot(i,v,'r*')
 						ev=qx.Queue(l)
+						out.append((i,RSI))
 		else:
 			try:
 				V[V.index(None)]=inp.get(False)
@@ -116,5 +181,6 @@ def eventDetection(a,sigma,l=30,p=0.01):
 				except qx.Empty:
 					continue
 			i=i+1
-		pl.plot(i,m,'ks')
-	pl.show()
+		#~ pl.plot(i,m,'ks')
+	#~ pl.show()
+	return out
--- regimShift.py
+++ regimShift.py
@@ -2,10 +2,12 @@ import numpy as num
 import pylab as pl
 from scipy.stats import t
 import math
+import Queue as qx
+from eventDetection import *
 
-def sigDiff(s,l,p=.1):
+def sigDiff(s,l,p=.01):
 	#calculate significant change for x with confidance p
-	tval=abs(t.ppf(p/2,2*l-2))
+	tval=abs(t.ppf(p/2,l-1))
 	return tval*s*math.sqrt(2.0/l)
 	
 
@@ -16,56 +18,91 @@ def test_event(diff,m,y):
 		return +1
 	if y-m<=diff:
 		return -1
-if __name__=='__main__':
-	l=20	# parameter of an algorithm
-	p=.05
-	mu, sigma, n = 0, 1, 300
-	a = num.random.normal(mu,sigma,n)
-	a[100:140]=a[100:140]-8
-	L=len(a)
-	vc=a[0:l]
-	pl.plot(range(L),a,'b-')
-	s=sigma
-	m=num.mean(vc)
-	diff=sigDiff(s,l)
-	print diff
-	i=l-1
-	fg=True
+		
+def qiHe(x,W=30,l=10,a=0.4,b=0.3):
+	# x is N-sample sequence
+	# W is window size
+	# a is outlier thereshold
+	# b is LS thereshold
+	# l is intial training < W 
+	N=len(x)
+	pl.plot(range(N),x,'b-')
+	c=W
+	vc=x[0:c]
+	fg=True 
+	loop=0
 	while fg:
-		if i+l>L:
-			break
-		j=i+1
-		ef=test_event(diff,m,a[j]) #event start flag
-		ep=1 #event prove flag
-		if ef==0:
-			pass
-		else:
-			RSI=0.0
-			for k in range(j,j+l):
-				if ef==-1 :
-					RSI=RSI+((m-diff)-a[j])/(l*s)
-				if ef==1 :
-					RSI=RSI-((m-diff)-a[j])/(l*s)
-				if RSI < 0 :
-					ep = 0
-					break
-		if ef != 0 and ep ==1 :
-			vc = a[j:(j+l)]
-			i=j+l-1
-			pl.plot(range(j,j+l),vc,'r*')
-		else :
-			vc = a[j:(j+l)]
-			i=j+l-1
-		m=num.mean(vc)
-		
-		
-			
-			
-				
-				
-		i=i+1
-		vc=a[(i-l):(i+1)]
+		m=num.median(vc)
 		s=num.std(vc)
-		m=num.mean(vc)
+		pl.plot([loop*W+i for i in range(W)],[float(m)]*W,'r--')
+		for i,w in enumerate(vc):
+			#~ pl.plot(loop*W+i,w,'r*')
+			if abs(w-m) > 3*s :
+				pl.plot(loop*W+i,w,'r*')
+		loop=loop+1
+		if (loop*W)+c > N :
+			break
+		vc=x[(loop*W):((loop*W)+c)]
+		
+		
+if __name__=='__main__':
+	l=30	# parameter of an algorithm
+	p=.01
+	N=3000
+	mu, sigma, n = 0, 1, N
+	a = num.random.normal(mu,sigma,n)
+	#~ diff=sigDiff(sigma,l)   # has to be replaced by t-statistics
+	#~ print diff
+	a[100:140]=a[100:140]-2
+	a[200:227]=a[200:227]+2
+	a[260]=-5.0
+	a[500:520]=a[500:520]+1
+	a[670:700]=a[670:700]-2
+	a[1000:1500]=a[1000:1500]+1
+	a[2000:2227]=a[2000:2227]-5
+	a[2227:2500]=a[2227:2500]+3
+	#~ a,ev1=genEvent(l,.1,1,.1)
+	#~ a2,ev2=genEvent(l,.2,4,.1)
+	#~ a=list(a)+list(a2)
+	#~ ev1=list(ev1)+list(ev2)
+	out=eventDetection(a,1,l,p)
+	pl.plot(range(len(a)),a)
+	pl.ylabel('Sequence Magnitude',fontsize=20)
+	pl.xlabel('Sample Number',fontsize=20)
+	pl.twinx()
+	#~ pl.plot([0,len(a)],[max(a),max(a)],'k',linewidth=2)
+	for w in out:
+		if w[1]>0 :
+			pl.vlines(w[0],0,abs(w[1]),color='green',linewidths=4)
+		else:
+			pl.vlines(w[0],0,abs(w[1]),color='red',linewidths=4)
+	pl.ylabel('Event Power',fontsize=20)
 	pl.show()
-	
+	#~ tp,fp=[],[]
+	#~ p=0.01
+	#~ l=30
+	#~ L=[10,15,20,25,30,35,40]
+	#~ es=[.25,.5,.75,1,1.25]
+	#~ ite=100
+	#~ tp=[]
+	#~ fp=[]
+	#~ for w in es:
+		#~ tp1=[0.0]*ite
+		#~ fp1=[0.0]*ite
+		#~ for it in range(ite): 
+			#~ a,ev1=genEvent(l,.1,w)
+			#~ r1=eventDetection(a,1,l,p)
+			#~ ev2=eventVector(r1,len(a))
+			#~ fp2,tp2 = roc(ev1,ev2)
+			#~ tp1[it]=tp2
+			#~ fp1[it]=fp2
+		#~ tp.append(num.mean(tp1))
+		#~ fp.append(num.mean(fp1))
+			
+			
+			
+	#~ pl.plot(es,tp)
+	#~ pl.plot(es,fp,'r')
+	#~ pl.plot(range(len(a)),ev,'r')
+	#~ pl.show()
+	#~ print roc([0,0,0,0,1,1,1,1,0,0,0,0],[0,0,0,1,1,1,1,1,0,0,0,0])
